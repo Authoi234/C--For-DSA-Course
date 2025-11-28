6759825121c147490de5041eb9587a0185698928
Authoi234 <jawadkhan.authoi@gmail.com>
2025-11-28 20:14:56 +0600

    string sorting and string constructor done

---
+++ module_6/string_constructor.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s = "Hello";
+    // string s("Hello");
+    // string s("Hello World", 2); // He
+    string s = "Hello World";
+    string t(s,2); // llo World
+    cout << t << endl;
+
+    return 0;
+}
